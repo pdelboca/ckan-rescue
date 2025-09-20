0eba48bec0744cb08f227114718b694f0f8572f7
Patricio Del Boca <patriciodelboca@gmail.com>
2025-09-20 10:14:56 -0300

    Migrate main action to click

--- src/ckan_rescue/cli.py
+++ src/ckan_rescue/cli.py
@@ -1,21 +1,27 @@
-import argparse
 import sys
+
+import click
 
 from .dcat import DCATDownloader
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Download data from a CKAN portal implementing ckanext-datajson")
-    parser.add_argument("url", help="URL of the data.json file")
-    parser.add_argument("-o", "--output", default="output", help="Output directory (default: output)")
-    parser.add_argument(
-        "-t", "--threads", type=int, default=5, help="Number of threads for parallel downloads (default: 5)"
-    )
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1.0")
+@click.group()
+@click.argument("url")
+@click.pass_context
+def cli(ctx, url):
+    ctx.obj = url
 
-    args = parser.parse_args()
 
-    downloader = DCATDownloader(args.url, args.output, args.threads)
+@cli.command()
+@click.option("--output", required=False, default="output", help="Output directory")
+@click.option("--threads", required=False, type=int, default=5, help="Number of threads for parallel downloads")
+@click.pass_obj
+def dcat_download(url, output, threads):
+    downloader = DCATDownloader(url, output, threads)
     success = downloader.run()
 
     sys.exit(0 if success else 1)
+
+
+def main():
+    cli()
